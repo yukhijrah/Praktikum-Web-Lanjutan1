fcdda665c1cf8a3bc50a5a1371b5dd75b8dbdfbd
root <kuatiseptiani>
2018-05-25 15:50:55 +0700

    Tambah method update

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,6 +7,9 @@
       <h1>Create New Posts</h1>
       <hr>
       <form method="POST" action="{{ route('posts.store')}}">
+
+        {{csrf_field() }}
+        
         <div class="form-group">
           <label for="usr">Title</label>
           <input type="text" class="form-control" id="title" name="title" value="{{ $post->title }}" required>
